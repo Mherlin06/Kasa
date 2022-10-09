82b46bc24581cf92f8e11df3c11afe87ee6acf5d
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-09 14:56:38 +0200

    add footer html

--- kasa-app/src/components/App.js
+++ kasa-app/src/components/App.js
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import Banner from "./Banner";
 import ThumbCards from "./ThumbCards";
-import Footer from "./Footer/Footer"
+import Footer from "./Footer"
 
 const App = () => (
   <div>
