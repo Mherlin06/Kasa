84708d8e528864b5f56a66a3e1b69f591823f058
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-09 17:44:20 +0200

    add logo to header & footer

--- kasa-app/src/components/Footer.js
+++ kasa-app/src/components/Footer.js
@@ -1,7 +1,17 @@
+import k from "../assets/logo/k-footer.svg";
+import house from "../assets/logo/house-footer.svg";
+import door from "../assets/logo/door-footer.svg";
+import s from "../assets/logo/s-footer.svg";
+import a from "../assets/logo/a-footer.svg";
+
 const Footer = () => (
   <footer>
     <div className="logo footer__logo">
-      <p>KASA</p>
+      <img src={k} alt="k-vector"></img>
+      <img src={house} alt="house-vector"></img>
+      <img src={door} alt="door-vector"></img>
+      <img src={s} alt="s-vector"></img>
+      <img src={a} alt="a-vector"></img>
     </div>
     <p className="copyrights">© 2020 Kasa. All rights reserved</p>
   </footer>
--- kasa-app/src/components/Header.js
+++ kasa-app/src/components/Header.js
@@ -1,13 +1,25 @@
-const Header = () => (
-  <header>
-    <div className='logo header__logo'>
-      <p>KASA</p>
-    </div>
-    <nav>
-      <a href="#">Acceuil</a>
-      <a href="#">A propos</a>
-    </nav>
-  </header>
-);
+import k from "../assets/logo/k-vector.svg";
+import house from "../assets/logo/house-vector.svg";
+import door from "../assets/logo/door-vector.svg";
+import s from "../assets/logo/s-vector.svg";
+import a from "../assets/logo/a-vector.svg";
+
+function Header() {
+  return (
+    <header>
+      <div className="logo header__logo">
+        <img src={k} alt="k-vector"></img>
+        <img src={house} alt="house-vector"></img>
+        <img src={door} alt="door-vector"></img>
+        <img src={s} alt="s-vector"></img>
+        <img src={a} alt="a-vector"></img>
+      </div>
+      <nav>
+        <a href="#">Acceuil</a>
+        <a href="#">A propos</a>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
