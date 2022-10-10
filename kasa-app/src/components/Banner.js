f324f3702e14bc9893b942d72deed7b467056ecb
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-10 21:30:34 +0200

    banner styles ok

--- kasa-app/src/components/Banner.js
+++ kasa-app/src/components/Banner.js
@@ -1,11 +1,14 @@
 import bannerImage from "../assets/photo-1593201464350-96ec57065aa2.webp";
 import "../styles/Banner.css";
 
-const Banner = () => (
-  <section className="banner">
-    <img src={bannerImage} alt="banner"></img>
-    <h1>Chez vous, partout et ailleurs</h1>
-  </section>
-);
+const Banner = () => {
+  const title = "Chez vous, partout et ailleurs";
+  return (
+    <section className="banner">
+      <img src={bannerImage} alt="banner"></img>
+      <h1>{title}</h1>
+    </section>
+  );
+};
 
 export default Banner;
