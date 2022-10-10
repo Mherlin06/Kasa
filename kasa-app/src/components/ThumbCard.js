ec90ccfca436b25c1bfa0a1e825b80484ed88e02
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-10 22:01:21 +0200

    thumcard styles pending

--- kasa-app/src/components/ThumbCard.js
+++ kasa-app/src/components/ThumbCard.js
@@ -1,9 +1,23 @@
 import "../styles/ThumbCard.css";
 
-const ThumbCard = () => (
-  <section className="ThumbCard">
-    <a href="#">Titre de la location</a>
-  </section>
-);
+const ThumbCard = () => {
+  let rentTitle = "Titre de la location";
+  return (
+    <section className="thumbcard">
+      <a href="#">
+        <h2>{rentTitle}</h2>
+      </a>
+      <a href="#">
+        <h2>{rentTitle}</h2>
+      </a>
+      <a href="#">
+        <h2>{rentTitle}</h2>
+      </a>
+      <a href="#">
+        <h2>{rentTitle}</h2>
+      </a>
+    </section>
+  );
+};
 
 export default ThumbCard;
