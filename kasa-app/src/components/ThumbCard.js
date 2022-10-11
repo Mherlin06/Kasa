0e24d19d898b40c4916f99c0052c921b1bdfb62a
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-11 19:15:14 +0200

    add locationList to thumbcard

--- kasa-app/src/components/ThumbCard.js
+++ kasa-app/src/components/ThumbCard.js
@@ -1,21 +1,15 @@
 import "../styles/ThumbCard.css";
+import { locationList } from "../data/locationList";
 
 const ThumbCard = () => {
-  let rentTitle = "Titre de la location";
   return (
     <section className="thumbcard">
-      <a href="#">
-        <h2>{rentTitle}</h2>
-      </a>
-      <a href="#">
-        <h2>{rentTitle}</h2>
-      </a>
-      <a href="#">
-        <h2>{rentTitle}</h2>
-      </a>
-      <a href="#">
-        <h2>{rentTitle}</h2>
-      </a>
+      {locationList.map((location) => (
+        <a href="#" key={location.id}>
+          <img key={location.id} src={location.cover} alt={location.title}></img>
+          <h2 key={location.id}>{location.title}</h2>
+        </a>
+      ))}
     </section>
   );
 };
