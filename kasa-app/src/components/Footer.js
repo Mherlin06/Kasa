95d5fa1aa70cb1c9d696ba35713614a46df87e1f
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-18 20:44:02 +0200

    change logo

--- kasa-app/src/components/Footer.js
+++ kasa-app/src/components/Footer.js
@@ -1,18 +1,10 @@
-import k from "../assets/logo/k-footer.svg";
-import house from "../assets/logo/house-footer.svg";
-import door from "../assets/logo/door-footer.svg";
-import s from "../assets/logo/s-footer.svg";
-import a from "../assets/logo/a-footer.svg";
+import logo from "../assets/logo/logo-footer.svg";
 import "../styles/Footer.css";
 
 const Footer = () => (
   <footer>
     <div className="logo">
-      <img src={k} alt="k-vector"></img>
-      <img src={house} alt="house-vector"></img>
-      <img src={door} alt="door-vector" className="door-vector"></img>
-      <img src={s} alt="s-vector"></img>
-      <img src={a} alt="a-vector"></img>
+    <img src={logo} alt="k-vector"></img>
     </div>
     <p className="copyrights">© 2020 Kasa. All rights reserved</p>
   </footer>
--- kasa-app/src/components/Header.js
+++ kasa-app/src/components/Header.js
@@ -1,24 +1,20 @@
 import { Link } from "react-router-dom";
-import k from "../assets/logo/k-vector.svg";
-import house from "../assets/logo/house-vector.svg";
-import door from "../assets/logo/door-vector.svg";
-import s from "../assets/logo/s-vector.svg";
-import a from "../assets/logo/a-vector.svg";
+import logo from "../assets/logo/logo-header.svg";
 import "../styles/Header.css";
 
 export default function Header() {
   return (
     <header>
       <div className="header__logo">
-        <img src={k} alt="k-vector"></img>
-        <img src={house} alt="house-vector"></img>
-        <img src={door} alt="door-vector" className="door-header"></img>
-        <img src={s} alt="s-vector"></img>
-        <img src={a} alt="a-vector"></img>
+      <img src={logo} alt="k-vector"></img>
       </div>
       <nav>
-        <Link to="/" className="nav-links">ACCUEIL</Link>
-        <Link to="/a-propos" className="nav-links">A PROPOS</Link>
+        <Link to="/" className="nav-links">
+          ACCUEIL
+        </Link>
+        <Link to="/a-propos" className="nav-links">
+          A PROPOS
+        </Link>
       </nav>
     </header>
   );
