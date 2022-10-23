5c55790afeff590e88031d5825c432bfedaee0ec
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-23 21:41:10 +0200

    add media queries to <dropdown>

--- kasa-app/src/pages/About.js
+++ kasa-app/src/pages/About.js
@@ -1,14 +1,16 @@
 import Banner from "../components/Banner";
 import Dropdown from "../components/Dropdown";
-import aboutInfos from "../data/aboutInfos"
+import aboutInfos from "../data/aboutInfos";
 
 const About = () => {
-    return (
-        <main>
-            <Banner class="banner about-banner"/>
-            {aboutInfos.map((infos) => <Dropdown label={infos.label} description={infos.description}/>)}
-        </main>
-    );
-}
+  return (
+    <main>
+      <Banner class="banner about-banner" />
+      {aboutInfos.map((infos) => (
+        <Dropdown label={infos.label} description={infos.description} />
+      ))}
+    </main>
+  );
+};
 
-export default About
+export default About;
