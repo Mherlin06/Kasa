f80657d5a082ea460747ed81b798ae5633c7055a
Hugues Mourice <mourice.hugues@gmail.com>
2022-10-31 21:01:01 +0100

    clean app.js

--- kasa-app/src/index.js
+++ kasa-app/src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Accomodation from "./pages/Accomodation";
+import Error from "./pages/Error";
 import "./styles/index.css";
-import App from "./App";
-import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/a-propos" element={<About />} />
+      <Route path="/fiche-logement/:id" element={<Accomodation />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+    <Footer />
   </BrowserRouter>
 );
