b6b23ca53c1c6ebe22099ecaf4030542c581e46c
Hugues Mourice <mourice.hugues@gmail.com>
2022-11-02 18:53:09 +0100

    last commit

--- kasa-app/src/index.js
+++ kasa-app/src/index.js
@@ -5,6 +5,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import IdChecker from "./components/IdChecker";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Accomodation from "./pages/Accomodation";
